61afef96b5ecb9ecbf1a1852f4f270832c578468
Oana Munteanu <hello2oana@yahoo.com>
2021-02-06 15:47:58 +0000

    add project init

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+
+function Application({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
+  
+  export default Application
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return (<div>Welcome you</div>);
+}
+
+export default Home
