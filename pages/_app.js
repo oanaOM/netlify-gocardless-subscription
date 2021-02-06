be905a743fed4839148e3930f0122ece073d332c
Oana Munteanu <hello2oana@yahoo.com>
2021-02-06 19:12:29 +0000

    add netlify identifier

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-function Application({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-  
-  export default Application
+import { IdentityContextProvider } from "react-netlify-identity-widget";
+
+function Application({ Component, pageProps, element }) {
+  const url = "https://nervous-minsky-4dcffc.netlify.app"
+
+  return (
+    <IdentityContextProvider url={url}>
+      <Component {...pageProps} />
+    </IdentityContextProvider>
+  );
+}
+
+export default Application;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import {useState} from 'react'
+import { IdentityModal, useIdentityContext } from 'react-netlify-identity-widget'
+import 'react-netlify-identity-widget/styles.css'
+import "@reach/tabs/styles.css"
+
+
 export default function Home() {
-    return (
-          <p className="description">
-            Get started by editing <code>pages/index.js</code>
-          </p>
-    )
-  }
+  const identity = useIdentityContext()
+  const [dialog, setDialog]= useState(false)
+  console.log(">>>", identity);
+  
+  return (
+    <>
+    {identity && identity.isLoggedIn && (
+      <div>
+        <h2>identity</h2>
+        <pre>{JSON.stringify(identity, null, 2)}</pre>
+      </div>
+    )}
+      <h1>Sign up for premium meal deal for dogs</h1>
+      <p>Get your subscription</p>
+      {!dialog && (<button onClick={()=>setDialog(true)}>Log in</button>)}
+      <IdentityModal
+          showDialog={dialog}
+          onCloseDialog={() => setDialog(false)}
+          onLogin={(user) => console.log('hello ', user?.user_metadata)}
+          onSignup={(user) => console.log('welcome ', user?.user_metadata)}
+          onLogout={() => console.log('bye ')}
+        />
+    </>
+  );
+}
