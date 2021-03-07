dd95359a314cf0626176756dea2cae6db93c1aef
Oana Munteanu <hello2oana@yahoo.com>
2021-03-07 21:09:37 +0000

    fix hello page

--- pages/hello.js
+++ pages/hello.js
@@ -2,15 +2,15 @@ import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
 
-useEffect(() => {
-    axios.get("./netlify/functions/get-hello")
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-    });
-}, []);
 
 export default function Hello () {
+    useEffect(() => {
+        axios.get("./netlify/functions/get-hello")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        });
+    }, []);
     return (
         <div>Hello</div>
     );
